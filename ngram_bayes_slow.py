a967c0a75541fae42345b913acf5748e7d0fd81d
markopmilo <marko.p.milovanovich@gmail.com>
2024-05-09 16:39:37 -0700

    no stopwords

--- ngram_bayes_slow.py
+++ ngram_bayes_slow.py
@@ -44,7 +44,7 @@ class NaiveBayes:
                 build = build + " " + line
             hamBuilder.append(build)
             file.close()
-        vectorizer = CountVectorizer(ngram_range=(1, self.n), stop_words='english')
+        vectorizer = CountVectorizer(ngram_range=(1, self.n))
         vectorizer.fit(hamBuilder)  # build ngram dictionary
         ngram = vectorizer.transform(hamBuilder)  # get ngram
         hamNgrams = vectorizer.vocabulary_
@@ -61,7 +61,7 @@ class NaiveBayes:
                 build = build + " " + line
             spamBuilder.append(build)
             file.close()
-        vectorizer = CountVectorizer(ngram_range=(1, self.n), stop_words='english')
+        vectorizer = CountVectorizer(ngram_range=(1, self.n))
         vectorizer.fit(spamBuilder)  # build ngram dictionary
         ngram = vectorizer.transform(spamBuilder)  # get ngram
         spamNgrams = vectorizer.vocabulary_
@@ -104,7 +104,7 @@ class NaiveBayes:
             line = line.translate(translating)
             line = line.strip().lower()
             line = line.replace('subject', '')
-            vectorizer = CountVectorizer(ngram_range=(1, self.n), stop_words='english')
+            vectorizer = CountVectorizer(ngram_range=(1, self.n))
             analyzer = vectorizer.build_analyzer()
             ngrams = analyzer(line)
             for ngram in ngrams:
