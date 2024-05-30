7d47b7615187ed18af0028351a454d017d403f80
Connor Sun <connorsun@gmail.com>
2024-05-29 21:06:15 -0700

    composite

--- composite_bayes.py
+++ composite_bayes.py
@@ -1,6 +1,7 @@
 import time
 from naive_bayes import NaiveBayes
 from ngram_bayes import NgramBayes
+from embeddings_bayes import EmbeddingsBayes
 
 class CompositeBayes(NaiveBayes):
     def __init__(self, bayeses):
@@ -19,11 +20,18 @@ class CompositeBayes(NaiveBayes):
 def create_composite_ngram(max_n):
     return [NgramBayes(i + 1) for i in range(max_n)]
 
+def create_composite_ngram_embedding():
+    return [NgramBayes(3), NgramBayes(4), EmbeddingsBayes()]
+
 if __name__ == "__main__":
     ts = time.time()
-    for n in range(1, 6):
-        nb = CompositeBayes(create_composite_ngram(n))
-        nb.train()
-        accuracy = nb.test()
-        print("accuracy for n = " + str(n) + " " + str(accuracy))
+    # for n in range(1, 6):
+    #     nb = CompositeBayes(create_composite_ngram(n))
+    #     nb.train()
+    #     accuracy = nb.test()
+    #     print("accuracy for n = " + str(n) + " " + str(accuracy))
+    nb = CompositeBayes(create_composite_ngram_embedding())
+    nb.train()
+    accuracy = nb.test()
+    print("accuracy " + str(accuracy))
     print("time taken " + str(time.time() - ts))
